2291fd14e2755992ed2c749912b91a288d0d5685
K-gultom <yehezkielgg28@gmail.com>
2024-06-28 19:09:52 +0700

    add [new commit]

---
+++ aplikasi2/resources/views/test.blade.php
@@ -0,0 +1,14 @@
+@extends('main')
+
+@section('content')
+    <div class="container-fluid">
+        
+        <h4 class="mb-3">Dashboard</h4>
+        <div class="text">
+            <ul class="text-decoration-none">
+                <li aria-current="page">Dashboard</li>
+            </ul>
+        </div>
+
+    </div>    
+@endsection
